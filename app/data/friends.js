f620c4128c1b7ecdf85ada50cf82d5b5ea3faa30
Eric Hamil <erichamil@Erics-MacBook-Pro.local>
2019-11-18 19:21:23 -0500

    created files and wrote files

---
+++ app/data/friends.js
@@ -0,0 +1,32 @@
+var friends = [
+  {
+  name: "Tye Sheridan",
+  photo: "http://www.gstatic.com/tv/thumb/persons/624958/624958_v9_bb.jpg",
+  scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 4]
+}, {
+  name: "Olivia Cooke",
+  photo: "http://www.gstatic.com/tv/thumb/persons/696404/696404_v9_bb.jpg",
+  scores: [4, 2, 5, 1, 3, 2, 2, 1, 3, 2]
+}, {
+  name: "Ben Mendelsohn",
+  photo: "http://www.gstatic.com/tv/thumb/persons/77856/77856_v9_ba.jpg",
+  scores: [5, 2, 2, 2, 4, 1, 3, 2, 5, 5]
+}, {
+  name: "Hannah John-Kamen",
+  photo: "http://www.gstatic.com/tv/thumb/persons/848796/848796_v9_bb.jpg",
+  scores: [3, 3, 4, 2, 2, 1, 3, 2, 2, 3]
+}, {
+  name: "T.J. Miller",
+  photo: "http://www.gstatic.com/tv/thumb/persons/508013/508013_v9_bb.jpg",
+  scores: [4, 3, 4, 1, 5, 2, 5, 3, 1, 4]
+}, {
+  name: "Win Morisaki",
+  photo: "http://www.gstatic.com/tv/thumb/persons/973529/973529_v9_ba.jpg",
+  scores: [4, 3, 4, 2, 4, 3, 5, 3, 5, 4]
+}, {
+  name: "Perdita Weeks",
+  photo: "http://www.gstatic.com/tv/thumb/persons/73134/73134_v9_bb.jpg", 
+  scores: [4, 4, 2, 3, 2, 2, 3, 2, 4, 5]
+}];
+
+module.exports = friends;
